53aa825892a5fea1339aa4e7e16170da2effea36
carlos <carloseduardoarnaud@gmail.com>
2023-06-18 23:24:13 -0300

    Post não nulo

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  context  "Testing factory" do
+  context "Testing factory" do
   it { expect(build(:post)).to be_valid }
+  end
+
+  context "Validating name" do
+    it "should be invalid if name nil" do
+      expect(build(:post, title:nil)).to be_invalid
+    end
   end
 end
