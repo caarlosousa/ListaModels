a23d2391d8036565fb85b28602f526e5d1c2e9cf
carlos <carloseduardoarnaud@gmail.com>
2023-06-19 18:46:41 -0300

    Teste Category 2

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  context "testing factory" do
+  context "Testing factory" do
     it {expect(build(:category)).to be_valid} 
+  end
+
+  context "Validating name" do
+    it "should be invalid if name nil" do
+      expect(build(:category, name:nil)).to be_invalid
+    end
+    it "should be invalid if repetead" do
+      create(:category, name: "test")
+      expect(build(:category, name: "test")).to be_invalid
+    end
   end
 end
