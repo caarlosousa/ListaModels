7f47d02ec057c8ffe864e5e8a3a75d1ed42eda86
carlos <carloseduardoarnaud@gmail.com>
2023-06-19 18:10:06 -0300

    Teste Commenatry 2

--- spec/models/commentary_spec.rb
+++ spec/models/commentary_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Commentary, type: :model do
   context "Testing factory" do
-    it "should create feedback" do
+    it "should create commentary" do
       create(:post, id:1)
-      expect(build(:feedback, post_id:1)).to be_valid
+      expect(build(:commentary, post_id:1)).to be_valid
+    end
+  end
+
+  context "Validating commentary" do
+    it "should be invalid if doesn't exist" do
+      expect(build(:commentary, post_id: -1)).to be_invalid
+    end
+    it "should be invalid if title nil" do
+      expect(build(:commentary, title:nil)).to be_invalid
     end
   end
 end
