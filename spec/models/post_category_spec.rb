4ef2bb373618bdc94f2e0dec13e6d6b39cb7a385
carlos <carloseduardoarnaud@gmail.com>
2023-06-19 18:24:29 -0300

    Teste PostCategory 3

--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -4,9 +4,8 @@ RSpec.describe PostCategory, type: :model do
   context "Testing factory" do
     it "should create postcategory" do
       create(:post, id:1)
-      expect(build(:post_category, post_id:1)).to be_valid
       create(:category, id:1)
-      expect(build(:post_category, category_id:1)).to be_valid
+      expect(build(:post_category, post_id:1, category_id:1)).to be_valid
     end
   end
 end
