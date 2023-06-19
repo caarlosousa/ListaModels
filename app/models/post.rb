52206e49a363b3b5ff27c7b9328bddf80c422874
carlos <carloseduardoarnaud@gmail.com>
2023-06-19 00:37:36 -0300

    Teste Title e Content de Post 2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
     has_many :posts, dependent: :destroy
 
-    validates :title, presence: :true, :content, presence: :true
+    validates :title, presence: :true 
+    validates :content, presence: :true
 end
