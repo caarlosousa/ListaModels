695dbfdb5a95f91b5df9128842108092922f1ddb
jessica8melo <jessica8melo@gmail.com>
2023-06-21 15:23:46 -0300

    ⚡️  improving post_category tests

--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe PostCategory, type: :model do
+  let(:category) { create(:category) }
+  let(:post) { create(:post) }
+  
   context "Testing factory" do
     it "should create postcategory" do
-      create(:post, id:1)
-      create(:category, id:1)
-      expect(build(:post_category, post_id:1, category_id:1)).to be_valid
+      expect(build(:post_category, post_id:post.id, category_id:category.id)).to be_valid
+    end
+  end
+  # É interessante testar para um id de categoria nula e para um id de post nulo. Tipo assim:
+  context 'Testing category' do
+    it 'should be invalid if nil' do
+      expect(build(:post_category, category: nil, post_id: post.id)).to be_invalid
+    end
+
+    it 'should be invalid if id does not exists' do
+      expect(build(:post_category, category_id: -1, post_id: post.id)).to be_invalid
     end
   end
 end
