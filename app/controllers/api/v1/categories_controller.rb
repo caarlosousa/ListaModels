a09429bde4e993a3ccbc7aeb8baa7e04c0159706
carlos <carloseduardoarnaud@gmail.com>
2023-06-26 01:06:40 -0300

    Testando Controller Categories 2

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::CategoriesController < ApplicationController
+    def index
+        categories = Category.all
+        render json: categories, status: :ok
+    end
+
+    def create
+        category = Category.new(category_params)
+        category.save!
+        render json: category, status: :created
+    rescue StandardError => e
+        render json: e, status: :bad_request
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name, :description)
+    end
 end
