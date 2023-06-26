6dab44bb63f91e53f29830835dbab98d0d03771f
carlos <carloseduardoarnaud@gmail.com>
2023-06-26 00:19:53 -0300

    Testando Controller Feedbacks 4

--- spec/requests/api/v1/feedbacks_spec.rb
+++ spec/requests/api/v1/feedbacks_spec.rb
@@ -27,10 +27,32 @@ RSpec.describe "Api::V1::Feedbacks", type: :request do
     let(:feedback_params) do
       attributes_for(:feedback)
     end
-    context "when feedback exists" do
+    context "when params are ok" do
       it "return http status ok" do
         patch "/api/v1/posts/#{post1.id}/feedbacks/update/#{feedback1.id}", params:{feedback: feedback_params}
         expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+  
+  describe "GET /show" do
+    let(:post1) {create(:post, title: "test", content: "testing")}
+    let(:feedback) { create(:feedback, like: true, post_id: post1.id)}
+
+    context "when the feedback exists" do
+      it "returns the feedback details" do
+        get "/api/v1/posts/#{post1.id}/feedbacks/show/#{feedback.id}"
+        expect(response).to have_http_status(:ok)
+
+        feedback_response = JSON.parse(response.body)
+        expect(feedback_response["like"]).to eq(true)
+      end
+    end
+
+    context "when the feedback does not exist" do
+      it "returns a not found error" do
+        get "/api/v1/posts/#{post1.id}/feedbacks/show/999"
+        expect(response).to have_http_status(:not_found)
       end
     end
   end
