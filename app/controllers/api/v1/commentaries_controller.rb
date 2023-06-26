855fdf6ffda7a20059a93dc47821e70f43f2a4f3
carlos <carloseduardoarnaud@gmail.com>
2023-06-26 00:38:07 -0300

    Testando Controller Commentaries 2

--- app/controllers/api/v1/commentaries_controller.rb
+++ app/controllers/api/v1/commentaries_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::CommentariesController < ApplicationController
+    def index
+        commentaries = Commentary.all
+        render json: commentaries, status: :ok
+    end
+
+    def create
+        post = Post.find(params[:id])
+        commentary = post.commentaries.create(commentary_params)
+        commentary.save!
+        render json: commentary, status: :created
+    rescue StandardError => e
+        render json: e, status: :bad_request
+    end
+    
+    private
+
+    def commentary_params
+        params.require(:commentary).permit(:content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ Rails.application.routes.draw do
         post ':id/feedbacks/create', to: 'feedbacks#create'
         patch ':id/feedbacks/update/:id', to: 'feedbacks#update'
         delete ':id/feedbacks/delete/:id', to: 'feedbacks#delete'
+        get ':id/commentaries/index', to: 'commentaries#index'
+        get ':id/commentaries/show/:id', to: 'commentaries#show'
+        post ':id/commentaries/create', to: 'commentaries#create'
+        patch ':id/commentaries/update/:id', to: 'commentaries#update'
+        delete ':id/commentaries/delete/:id', to: 'commentaries#delete'
       end
     end
   end
